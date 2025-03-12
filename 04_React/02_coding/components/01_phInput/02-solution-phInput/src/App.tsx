f0bf19beba9ea75564dddf45f5c655d3b5756025
kirandash <thekirankumardash@gmail.com>
2025-03-12 18:26:10 +0800

    remove css

--- 04_React/02_coding/components/01_phInput/02-solution-phInput/src/App.tsx
+++ 04_React/02_coding/components/01_phInput/02-solution-phInput/src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState("");
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(formatPhNumber(event.target.value));
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input
+        type="tel"
+        value={value}
+        onChange={onChange}
+        placeholder="(+65) 9999 9999"
+      />
+      <button onClick={() => setValue("")} disabled={value.length === 0}>
+        X
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+const formatPhNumber = (phNumberRaw: string) => {
+  // remove non digits
+  // format the ph number
+
+  let output = "";
+  // capital D: non digits, small d: digits
+  const str = phNumberRaw.replace(/[^\d+]/g, "");
+
+  // formatting
+  if (str.length > 0) {
+    output += "(";
+    output += str.substring(0, 3);
+  }
+
+  if (str.length > 3) {
+    output += ") ";
+    output += str.substring(3, 7);
+  }
+
+  if (str.length > 7) {
+    output += " ";
+    output += str.substring(7, 11);
+  }
+  return output;
+};
